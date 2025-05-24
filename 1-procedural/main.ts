14be35494b9ec86a85366b274487a06ab9176782
Aleksandr Andreyko <a.andreyko@keep-calm.ru>
2025-05-24 13:30:57 +0400

    задание 1 готово

--- 1-procedural/main.ts
+++ 1-procedural/main.ts
@@ -1,130 +1,166 @@
 // Процедурный калькулятор
-import "lodash";
+type calcInstanceState = {
+  display: HTMLElement | null;
+  expressionElement: HTMLElement | null;
+  history: HTMLElement | null;
+  currentNumber: string;
+  firstOperand: number | null;
+  operator: string | null;
+};
 
-let display: HTMLElement | null = null;
-let expressionElement: HTMLElement | null = null;
-let currentNumber: string = "0";
-let firstOperand: number | null = null;
-let operator: string | null = null;
+type logRecord = {
+  expression: string;
+  result: string;
+  recType: string;
+};
+
+const calc1: calcInstanceState = {
+  display: null,
+  expressionElement: null,
+  history: null,
+  currentNumber: '0',
+  firstOperand: null,
+  operator: null,
+};
+
+const calc2: calcInstanceState = {
+  display: null,
+  expressionElement: null,
+  history: null,
+  currentNumber: '0',
+  firstOperand: null,
+  operator: null,
+};
 
 // Обновление отображения
-function updateDisplay() {
-  if (display) {
-    display.textContent = currentNumber;
+function updateDisplay(state: calcInstanceState) {
+  if (state.display) {
+    state.display.textContent = state.currentNumber;
   }
 }
 
 // Обновление выражения
-function updateExpression() {
-  if (expressionElement) {
-    if (firstOperand !== null && operator !== null) {
-      expressionElement.textContent = `${firstOperand} ${operator}`;
+function updateExpression(state: calcInstanceState) {
+  if (state.expressionElement) {
+    if (state.firstOperand !== null && state.operator !== null) {
+      state.expressionElement.textContent = `${state.firstOperand} ${state.operator}`;
     } else {
-      expressionElement.textContent = "";
+      state.expressionElement.textContent = '';
     }
   }
 }
 
 // Добавление цифры
-function appendNumber(num: string) {
-  if (currentNumber === "0") {
-    currentNumber = num;
+function appendNumber(num: string, state: calcInstanceState) {
+  if (state.currentNumber === '0') {
+    state.currentNumber = num;
   } else {
-    currentNumber += num;
+    state.currentNumber += num;
   }
-  updateDisplay();
+  updateDisplay(state);
 }
 
 // Очистка калькулятора
-function clear() {
-  currentNumber = "0";
-  firstOperand = null;
-  operator = null;
-  updateDisplay();
-  updateExpression();
+function clear(state: calcInstanceState) {
+  state.currentNumber = '0';
+  state.firstOperand = null;
+  state.operator = null;
+  updateDisplay(state);
+  updateExpression(state);
 }
 
 // Выполнение операции
-function performOperation(isOperator: boolean = false) {
-  if (firstOperand === null || operator === null) return;
+function performOperation(isOperator: boolean = false, state: calcInstanceState) {
+  if (state.firstOperand === null || state.operator === null) return;
 
-  const secondOperand = parseFloat(currentNumber);
+  const secondOperand = parseFloat(state.currentNumber);
   let result: number;
 
-  switch (operator) {
-    case "+":
-      result = firstOperand + secondOperand;
+  switch (state.operator) {
+    case '+':
+      result = state.firstOperand + secondOperand;
+      logHistory({ expression: `${state.firstOperand} + ${secondOperand}`, result: result.toString(), recType: 'add' }, state);
       break;
-    case "-":
-      result = firstOperand - secondOperand;
+    case '-':
+      result = state.firstOperand - secondOperand;
+      logHistory({ expression: `${state.firstOperand} - ${secondOperand}`, result: result.toString(), recType: 'subtract' }, state);
       break;
-    case "/":
+    case '/':
       if (secondOperand === 0) {
-        alert("Ошибка: деление на ноль");
-        clear();
+        logHistory({ expression: `${state.firstOperand} / ${secondOperand}`, result: 'NaN', recType: 'error' }, state);
+        alert('Ошибка: деление на ноль');
+        clear(state);
         return;
       }
-      result = firstOperand / secondOperand;
+      result = state.firstOperand / secondOperand;
+      logHistory({ expression: `${state.firstOperand} / ${secondOperand}`, result: result.toString(), recType: 'divide' }, state);
+      break;
+    case '*':
+      result = state.firstOperand * secondOperand;
+      logHistory({ expression: `${state.firstOperand} * ${secondOperand}`, result: result.toString(), recType: 'multiply' }, state);
       break;
     default:
       return;
   }
 
   if (!isOperator) {
-    currentNumber = result.toString();
-    firstOperand = null;
-    operator = null;
-    updateDisplay();
-    updateExpression();
+    state.currentNumber = result.toString();
+    state.firstOperand = null;
+    state.operator = null;
+    updateDisplay(state);
+    updateExpression(state);
   } else {
-    firstOperand = result;
-    currentNumber = "0";
+    state.firstOperand = result;
+    state.currentNumber = '0';
   }
+}
+
+// Запись в историю
+function logHistory({ expression, result, recType }: logRecord, state: calcInstanceState) {
+  const historyItem: HTMLLIElement = document.createElement('li');
+  historyItem.textContent = `${expression} = ${result}`;
+  historyItem.classList.add('history-item', recType);
+  state.history?.appendChild(historyItem);
 }
 
 // Установка оператора
-function setOperator(op: string) {
-  if (firstOperand === null) {
-    firstOperand = parseFloat(currentNumber);
-    currentNumber = "0";
-  } else if (operator !== null && currentNumber !== "0") {
-    performOperation(true);
+function setOperator(op: string, state: calcInstanceState) {
+  if (state.firstOperand === null) {
+    state.firstOperand = parseFloat(state.currentNumber);
+    state.currentNumber = '0';
+  } else if (state.operator !== null && state.currentNumber !== '0') {
+    performOperation(true, state);
   }
-  operator = op;
-  updateDisplay();
-  updateExpression();
+  state.operator = op;
+  updateDisplay(state);
+  updateExpression(state);
 }
 
 // Инициализация калькулятора
-function initCalculator() {
-  display = document.getElementById("display");
-  expressionElement = document.getElementById("expression");
-  clear();
+function initCalculator(state: calcInstanceState, preffix: string) {
+  state.display = document.getElementById(`${preffix}-display`);
+  state.expressionElement = document.getElementById(`${preffix}-expression`);
+  state.history = document.getElementById(`${preffix}-history`);
+  clear(state);
 
   // Добавляем обработчики для цифровых кнопок
   for (let i = 0; i <= 9; i++) {
-    document
-      .getElementById(`num${i}`)
-      ?.addEventListener("click", () => appendNumber(i.toString()));
+    document.getElementById(`${preffix}-num${i}`)?.addEventListener('click', () => appendNumber(i.toString(), state));
   }
 
   // Добавляем обработчики для операторов
-  document
-    .getElementById("addButton")
-    ?.addEventListener("click", () => setOperator("+"));
-  document
-    .getElementById("subtractButton")
-    ?.addEventListener("click", () => setOperator("-"));
-  document
-    .getElementById("divideButton")
-    ?.addEventListener("click", () => setOperator("/"));
+  document.getElementById(`${preffix}-addButton`)?.addEventListener('click', () => setOperator('+', state));
+  document.getElementById(`${preffix}-subtractButton`)?.addEventListener('click', () => setOperator('-', state));
+  document.getElementById(`${preffix}-divideButton`)?.addEventListener('click', () => setOperator('/', state));
+  document.getElementById(`${preffix}-multiplyButton`)?.addEventListener('click', () => setOperator('*', state));
 
   // Добавляем обработчики для специальных кнопок
-  document.getElementById("clearButton")?.addEventListener("click", clear);
-  document
-    .getElementById("equalsButton")
-    ?.addEventListener("click", () => performOperation());
+  document.getElementById(`${preffix}-clearButton`)?.addEventListener('click', () => clear(state));
+  document.getElementById(`${preffix}-equalsButton`)?.addEventListener('click', () => performOperation(false, state));
 }
 
 // Инициализация при загрузке страницы
-document.addEventListener("DOMContentLoaded", initCalculator);
+document.addEventListener('DOMContentLoaded', () => {
+  initCalculator(calc1, 'calc1');
+  initCalculator(calc2, 'calc2');
+});
